1f26e6375476407a7a8db99b62fd97b1426287af
Elisa ROGGE <elisa.rogge59@gmail.com>
2023-05-05 10:59:43 +0200

    check number, radio and checkbox

--- modal.js
+++ modal.js
@@ -30,14 +30,14 @@ function closeModal() {
 
 
 function checkFirstname() {
-  const regFirstName = /^[a-zA-Z]{2,}$/;
-  const firstname = document.getElementById('first').value;
+  const regFirstName = /^[a-zA-Z]{2,}(?:['\s-][a-zA-Z]+)*$/;
+  const firstname = document.getElementById('first');
   const error = document.getElementById('errorFirstname');
 
-  if (!regFirstName.test(firstname)) {
+  if (!regFirstName.test(firstname.value)) {
     error.style.display = 'block';
     error.textContent = 'Veuillez inscrire un prénom correct';
-    document.getElementById('first').focus();
+    firstname.focus();
     return false;
   } else {
     error.style.display = 'none'
@@ -47,13 +47,13 @@ function checkFirstname() {
 
 function checkLastName() {
   const regLastName = /^[a-zA-Z]{2,}(?:['\s-][a-zA-Z]+)*$/;
-  const lastName = document.getElementById('last').value;
+  const lastName = document.getElementById('last');
   const error = document.getElementById('errorLast');
 
-  if (!regLastName.test(lastName)) {
+  if (!regLastName.test(lastName.value)) {
     error.style.display = 'block';
     error.textContent = 'Veuillez inscrire un nom de famille correct';
-    document.getElementById('last').focus();
+    lastName.focus();
     return false;
   } else {
     error.style.display = 'none'
@@ -62,14 +62,14 @@ function checkLastName() {
 }
 
 function checkEmail() {
-  const regEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const email = document.getElementById('email').value;
+  const regEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  const email = document.getElementById('email');
   const error = document.getElementById('errorEmail');
 
-  if (!regEmail.test(email)) {
+  if (!regEmail.test(email.value)) {
     error.style.display = 'block'
     error.textContent = 'Veuillez inscrire une adresse e-mail valide';
-    document.getElementById('email').focus();
+    email.focus();
     return false;
   } else {
     error.style.display = 'none'
@@ -78,17 +78,19 @@ function checkEmail() {
 }
 
 function checkDate() {
-  const date = document.getElementById('birthdate').value;
+  const date = document.getElementById('birthdate');
   const error = document.getElementById('errorDate');
 
-  const inputDate = new Date(date);
+  const inputDate = new Date(date.value);
+  const yearMax = new Date().getFullYear() - 13 ;
+  console.log(yearMax)
 
   const inputYear = inputDate.getFullYear();
 
-  if (inputYear > 2010) {
+  if (inputYear > yearMax) {
     error.style.display = 'block'
     error.textContent = 'Vous devez avoir plus de 13 ans';
-    document.getElementById('date').focus();
+    date.focus();
     return false;
   } else {
     error.style.display = 'none'
@@ -96,6 +98,59 @@ function checkDate() {
   }
 }
 
+function checkNumber() {
+  const regNumbers = /^\d+$/;
+
+  const input = document.getElementById('quantity');
+  const error = document.getElementById('errorNumber');
+  
+  if (!regNumbers.test(input.value)) {
+    error.style.display = 'block'
+    error.textContent = 'Veuillez inscrire uniquement des chiffres';
+    input.focus();
+    return false;
+  } else {
+    error.style.display = 'none'
+    return true;
+  }
+}
+
+function checkRadio() {
+  const radioButtons = document.getElementsByName('location');
+  const error = document.getElementById('errorRadio');
+  
+  let selected = false;
+
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+      selected = true;
+      break;
+    }
+  }
+  
+  if (!selected) {
+    error.style.display = 'block'
+    error.textContent = 'Veuillez sélectionner un lieu';
+    return false;
+  } else {
+    error.style.display = 'none'
+    return true;
+  }
+}
+
+function checkCheckbox(){
+  const checkbox1 = document.getElementById('checkbox1')
+  const error = document.getElementById('errorCheckbox')
+
+  if(!checkbox1.checked){
+    error.style.display = 'block'
+    error.textContent = 'Vous devez accepter les conditions générales d\'utilisation'
+    return false
+  }else{
+    error.style.display = 'none'
+    return true;
+  }
+}
 
 // formModal
 const formModal = document.getElementById('formModal')
@@ -105,15 +160,31 @@ formModal.addEventListener('submit', (e) => validate(e))
 // })
 
 function validate(e){
- e.preventDefault()
+  e.preventDefault()
 
-  if(checkFirstname() && checkLastName() && checkEmail() && checkDate()){
-        //Afficher les champs dans la console.log
-        console.log("Tout est ok")
-        return true;
+  if(checkFirstname() && checkLastName() && checkEmail() && checkDate() && checkNumber() && checkRadio() && checkCheckbox()){
+    // Toutes tes valeurs
+    const firstname = document.getElementById('first').value;
+    const lastname = document.getElementById('last').value;
+    const email = document.getElementById('email').value;
+    const birthdate = document.getElementById('birthdate').value;
+    const quantity = document.getElementById('quantity').value;
+
+    const contact = {
+      firstname,
+      lastname,
+      email,
+      birthdate,
+      quantity
+    }
+
+    // Les afficher dans un console.log(contact)
+    console.log(contact)
+      console.log("Tout est ok")
+      return true;
     }else{
       console.log("Erreur")
-        return false;
+      return false;
     }
 
 }
